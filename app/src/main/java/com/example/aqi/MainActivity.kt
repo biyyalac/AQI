3781329e4e8b503ed12f0b61d33617131f2fb1a0
Biyyala Chandramouli <biyyalac@google.com>
2022-11-09 11:39:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aqi/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.aqi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+     var textview: TextView?=null
+  //  lateinit var d:String;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //val k=10/0
+
+    }
+}
